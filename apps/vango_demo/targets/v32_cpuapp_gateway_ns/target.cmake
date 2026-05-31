966f367b92b22d766c40522384689b11d031ba29
800900 <992878191@qq.com>
2026-05-31 22:03:26 +0800

    ne

---
+++ apps/vango_demo/targets/v32_cpuapp_gateway_ns/target.cmake
@@ -0,0 +1,5 @@
+# v32_cpuapp_gateway_ns Profile Configuration
+
+set(BOARD v32f20x_board/v32f20x/cpuapp/cpuapp_ns)
+set(CONF_FILE ${CMAKE_CURRENT_LIST_DIR}/prj.conf)
+set(DTC_OVERLAY_FILE ${CMAKE_CURRENT_LIST_DIR}/app.overlay)
